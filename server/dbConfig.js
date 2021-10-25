3ef475f59259a5be2f8262fc0fb1a326fb787a98
aarushirai2210 <aarushirai_2000@outlook.com>
2021-10-25 13:43:07 +0530

    db configured

---
+++ server/dbConfig.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+var dbconnect = mysql.createConnection({
+    host:process.env.HOST,
+    port:process.env.DBPORT,
+    user:"admin",
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+  });
+
+dbconnect.connect(function(err) {
+    if (err) throw err;
+    console.log("Database Connected!");
+});
+
+module.exports = {dbconnect}
